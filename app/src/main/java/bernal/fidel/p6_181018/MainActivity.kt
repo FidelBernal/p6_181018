f3caf7a7f01207406c454591165392b21c8b7be8
Fidel <fideljbernalz@gmail.com>
2022-03-02 13:27:27 -0700

    Practica 06 181018

---
+++ app/src/main/java/bernal/fidel/p6_181018/MainActivity.kt
@@ -0,0 +1,11 @@
+package bernal.fidel.p6_181018
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/bernal/fidel/p6_181018/Pelicula.kt
@@ -0,0 +1,3 @@
+package bernal.fidel.p6_181018
+
+data class Pelicula(var titulo:String, var image:Int, var header:Int, var sinopsis:String)
